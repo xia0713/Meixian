ff7a2ef9ce25d174648c354d20e5c3c0e236ceee
夏卫超 <1102384692@qq.com>
2018-04-20 11:52:38 +0800

    第一次上传

--- app/src/main/java/com/shanghui/meixian/actiivity/HuiyuanDetailActivity.java
+++ app/src/main/java/com/shanghui/meixian/actiivity/HuiyuanDetailActivity.java
@@ -55,7 +55,6 @@ public class HuiyuanDetailActivity extends BaseNetActivity {
 
     @Override
     public void setData() {
-
     }
 
     @OnClick({R.id.back_layout, R.id.right_image, R.id.iv_share, R.id.iv_call, R.id.iv_address, R.id.iv_tag, R.id.ll_jianjie, R.id.ll_chanpin})
